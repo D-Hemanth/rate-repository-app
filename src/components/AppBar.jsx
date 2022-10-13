0c18314bf781b105ad6eb7a02450979735618e84
Hemanth-D <hemanthh531@gmail.com>
2022-10-13 16:42:22 +0530

    part10 10.4: Add an Appbar component to navigate between views in app & add to main file

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,26 @@
+import { View, StyleSheet, Pressable } from 'react-native';
+import Constants from 'expo-constants';
+import Text from './Text';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    // ...
+  },
+  // ...
+});
+
+const AppBar = () => {
+  return (
+    <View style={styles.container}>
+      <Pressable>
+        <Text
+        >
+          Repositories
+        </Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default AppBar;
