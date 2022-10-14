694196855660005c064cf181b96467cad798087a
Hemanth-D <hemanthh531@gmail.com>
2022-10-14 13:56:19 +0530

    part10 10.5: Modify counts>1000 to display with unit k, make UI better with author image & styling

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,39 +1,80 @@
-import { View, Text, Image, StyleSheet } from 'react-native';
+import Text from './Text';
 
 const styles = StyleSheet.create({
   tinyLogo: {
-    width: 50,
-    height: 50,
+    width: 40,
+    height: 40,
     borderRadius: 5,
   },
   viewCard: {
     display: 'flex',
+    padding: 15,
+    backgroundColor: '#e1e4e8',
   },
-  viewCardItem: {
+  viewCardItemA: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  viewCardItemA1: {
+    display: 'flex',
+    padding: 10,
+  },
+  viewCardItemB: {
+    display: 'flex',
+  },
+  viewCardItemB1: {
     flexDirection: 'row',
     justifyContent: 'space-evenly',
-    alignContent: 'center',
-    // flexWrap: 'wrap',
+  },
+  viewCardItemB2: {
+    justifyContent: 'space-evenly',
   },
 });
 
 const RepositoryItem = ({ item }) => {
+  // Counts, such as number of stars and forks, larger than or equal to 1000 should be displayed in thousands with precision of one decimal and with a "k" suffix
+  const formattedNumber = (number) => {
+    const round = (number) => Math.round(number * 10) / 10;
+
+    if (typeof number !== 'number') return null;
+    if (number < 1000) return number;
+    return `${round(number / 1000)}k`;
+  };
+
   // console.log("item from data", item);
   return (
     <View style={styles.viewCard}>
-      <View style={styles.viewCardItem}>
+      <View style={styles.viewCardItemA}>
         <Image style={styles.tinyLogo} source={{ uri: item.ownerAvatarUrl }} />
-        <View>
-          <Text>{item.fullName}</Text>
-          <Text>{item.description}</Text>
-          <Text>{item.language}</Text>
+        <View style={styles.viewCardItemA1}>
+          <Text fontSize="subheading" fontWeight="bold">
+            {item.fullName}
+          </Text>
+          <Text color="textSecondary">{item.description}</Text>
+          <Text>
+            <Text backgroundColor="backgroundBlueColor">{item.language}</Text>
+          </Text>
         </View>
       </View>
-      <View>
-        <Text>{item.stargazersCount}</Text>
-        <Text>{item.forksCount}</Text>
-        <Text>{item.reviewCount}</Text>
-        <Text>{item.ratingAverage}</Text>
+      <View style={styles.viewCardItemB}>
+        <View style={styles.viewCardItemB1}>
+          <View style={styles.viewCardItemB2}>
+            <Text>{formattedNumber(item.stargazersCount)}</Text>
+            <Text color="textSecondary">Stars</Text>
+          </View>
+          <View style={styles.viewCardItemB2}>
+            <Text>{formattedNumber(item.forksCount)}</Text>
+            <Text color="textSecondary">Forks</Text>
+          </View>
+          <View style={styles.viewCardItemB2}>
+            <Text>{formattedNumber(item.reviewCount)}</Text>
+            <Text color="textSecondary">Reviews</Text>
+          </View>
+          <View style={styles.viewCardItemB2}>
+            <Text>{formattedNumber(item.ratingAverage)}</Text>
+            <Text color="textSecondary">Rating</Text>
+          </View>
+        </View>
       </View>
     </View>
   );
