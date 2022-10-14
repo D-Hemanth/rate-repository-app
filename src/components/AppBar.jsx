58138bad0e022de08eedf5e8a1e75a8a485f0483
Hemanth-D <hemanthh531@gmail.com>
2022-10-14 15:15:19 +0530

    part10 10.7: Add ScrollView to make the appbar scrollable horizontally once tabs don't fit screen

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import AppBarTab from './AppBarTab';
 
@@ -13,8 +13,10 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab tabName="Repositories" tabView="/" />
-      <AppBarTab tabName="SignIn" tabView="signIn" />
+      <ScrollView horizontal>
+        <AppBarTab tabName="Repositories" tabView="/" />
+        <AppBarTab tabName="SignIn" tabView="signIn" />
+      </ScrollView>
     </View>
   );
 };
