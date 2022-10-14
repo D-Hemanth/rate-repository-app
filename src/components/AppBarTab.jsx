576070607d141ba9d3ac7dbe4595893f8b1661fb
Hemanth-D <hemanthh531@gmail.com>
2022-10-14 13:53:12 +0530

    part10 10.5: Increase the navigation tab Appbar padding

--- src/components/AppBarTab.jsx
+++ src/components/AppBarTab.jsx
@@ -1,11 +1,18 @@
-import { Pressable, View } from 'react-native';
+import { Pressable, View, StyleSheet } from 'react-native';
 import Text from './Text';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+});
 
 const AppBarTab = ({ tabName }) => {
   return (
     <View>
       <Pressable>
         <Text
+          style={styles.container}
           fontWeight="bold"
           fontSize="subheading"
           backgroundColor="backgroundBlueColor"
