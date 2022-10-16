16aaeabf56b83aea4d94ffd9b6cc5de71e08e2c7
Hemanth-D <hemanthh531@gmail.com>
2022-10-16 22:20:04 +0530

    part10 10.11: Complete fetching repositories with apollo client

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -4,12 +4,25 @@ import { GET_REPOSITORIES } from '../graphql/queries';
 import Text from '../components/Text';
 
 const useRepositories = () => {
-  const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
+  // const [repositories, setRepositories] = useState();
+  // const [loading, setLoading] = useState(false);
 
-  // get data from the rate repository api server backend using apollo server with sqLite & use fetch() method to fetch data from the server
-  const fetchRepositories = async () => {
-    setLoading(true);
+  // // get data from the rate repository api server backend using apollo server with sqLite & use fetch() method to fetch data from the server
+  // const fetchRepositories = async () => {
+  //   setLoading(true);
+
+  //   // Replace the IP address part with your own IP address!
+  //   const response = await fetch('http://192.168.1.8:5000/api/repositories');
+  //   const json = await response.json();
+
+  //   setLoading(false);
+  //   setRepositories(response);
+  // };
+
+  // // use useEffect to call the fetch repositories function on every refresh
+  // useEffect(() => {
+  //   fetchRepositories();
+  // }, []);
 
   // use graphql query to get the rate repository data from apollo server backend
   const { data, loading, ...result } = useQuery(GET_REPOSITORIES, {
