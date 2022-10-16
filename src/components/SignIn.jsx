54935f900aecb51805ddbdc911785d0f3e41b533
Hemanth-D <hemanthh531@gmail.com>
2022-10-16 11:20:17 +0530

    part10 10.8: Create SignIn component to use formiktextinput & textinput components to make signIn form

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,41 @@
-import Text from './Text';
+import FormikTextInput from './FormikTextInput';
+import { Formik } from 'formik';
+import { View, StyleSheet } from 'react-native';
+import Button from './Button';
+
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+const styles = StyleSheet.create({
+  button: {
+    margin: 15,
+  },
+});
+
+const SignInForm = ({ onSubmit }) => {
+  return (
+    <View>
+      <FormikTextInput name="username" placeholder="Username" />
+      <FormikTextInput name="password" placeholder="Password" secureTextEntry />
+      <Button onPress={onSubmit} style={styles.button}>
+        Sign In
+      </Button>
+    </View>
+  );
+};
 
 const SignIn = () => {
-  return <Text>The sign in view</Text>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
 };
 
 export default SignIn;
