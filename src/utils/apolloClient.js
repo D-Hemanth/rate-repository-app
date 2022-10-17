fa9220e8bf4e7fd403506ff8dff6c9d7537ae7ca
Hemanth-D <hemanthh531@gmail.com>
2022-10-17 16:34:15 +0530

    part10 10.15: Use useContext with auth headers token in createApolloClient along with gql link

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,18 +1,39 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import Constants from 'expo-constants';
+import { setContext } from '@apollo/client/link/context';
+
+// You might need to change this depending on how you have configured the Apollo Server's URI
+const { apollo_uri } = Constants.manifest.extra;
 
 // HTTP connection to the GraphQL server
 const httpLink = createHttpLink({
-  // Replace the IP address part with your own IP address!
-  // URL used to connect to the Apollo Server is otherwise the same as the one you used with the Fetch API expect the port is 4000 and the path is /graphql
-  uri: Constants.manifest.extra.apollo_uri,
+  uri: apollo_uri,
 });
 
 // create a new client object using ApolloClient, which is then used to send a query to the server
 // The link parameter given to the client object defines how apollo connects to the gql server.
-const createApolloClient = () => {
+const createApolloClient = (authStorage) => {
+  // we will send the access token to Apollo Server in each request in the Authorization header
+  // in the format Bearer <ACCESS_TOKEN> by using the setContext function
+  const authLink = setContext(async (_, { headers }) => {
+    try {
+      const accessToken = await authStorage.getAccessToken();
+      return {
+        headers: {
+          ...headers,
+          authorization: accessToken ? `Bearer ${accessToken}` : '',
+        },
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        headers,
+      };
+    }
+  });
+
   return new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
   });
 };
