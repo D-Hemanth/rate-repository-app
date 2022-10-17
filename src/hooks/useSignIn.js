845d2c533e12df535a3baee574a2ea6a579d7b3b
Hemanth-D <hemanthh531@gmail.com>
2022-10-17 16:40:24 +0530

    part10 10.15: Modify useSignIn to use authStorage, apolloClient to setAccessToken, resetStore after mutation

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,7 +1,12 @@
-import { useMutation } from '@apollo/client';
+import { useApolloClient, useMutation } from '@apollo/client';
 import { LOGIN } from '../graphql/mutations';
+import useAuthStorage from './useAuthStorage';
 
 const useSignIn = () => {
+  const authStorage = useAuthStorage();
+  // use apolloClient to use resetStore() method & remove all AsyncStorage data on the users device
+  const apolloClient = useApolloClient();
+
   // useSignIn hook that sends the authenticate mutation using the useMutation hook
   const [mutate, result] = useMutation(LOGIN, {
     onError: (error) => {
@@ -12,7 +17,12 @@ const useSignIn = () => {
   const signIn = async ({ username, password }) => {
     // call the mutate function here with the right arguments which gives output { data, loading, error }
     // use mutate function query from useMutation for logging in a user who's data is already in gql server
-    return await mutate({ variables: { username, password } });
+    const { data } = await mutate({ variables: { username, password } });
+    await authStorage.setAccessToken(data.authenticate.accessToken);
+    apolloClient.resetStore();
+
+    // console.log('Logged in user accessToken', data);
+    return data;
   };
 
   return [signIn, result];
