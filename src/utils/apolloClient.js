6ba0dc7a7a5a9eb77977dc31471a8d1687705965
Hemanth-D <hemanthh531@gmail.com>
2022-10-17 10:23:43 +0530

    part10 10.12: Modify apolloClient to use apollo_uri from the env file instead of hardcoding it

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,10 +1,11 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Constants from 'expo-constants';
 
 // HTTP connection to the GraphQL server
 const httpLink = createHttpLink({
   // Replace the IP address part with your own IP address!
   // URL used to connect to the Apollo Server is otherwise the same as the one you used with the Fetch API expect the port is 4000 and the path is /graphql
-  uri: 'http://192.168.1.8:4000/graphql',
+  uri: Constants.manifest.extra.apollo_uri,
 });
 
 // create a new client object using ApolloClient, which is then used to send a query to the server
