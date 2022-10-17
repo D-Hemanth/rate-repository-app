78f1e4d6ce059efa91ed071e7c9c0fe667e22e63
Hemanth-D <hemanthh531@gmail.com>
2022-10-17 14:50:36 +0530

    part10 10.14: Create authoStorage component methods getAccessToken, setAccessToken, removeAccessToken use AsynStorage

---
+++ src/utils/authStorage.js
@@ -0,0 +1,25 @@
+
+class AuthStorage {
+  // Because AsyncStorage keys are global, it is usually a good idea to add a namespace for the keys.
+  // In this context, the namespace is just a prefix we provide for the storage abstraction's keys.
+  constructor(namespace = 'auth') {
+    this.namespace = namespace;
+  }
+
+  getAccessToken() {
+    // Get the access token for the storage
+    return AsyncStorage.getItem(`${this.namespace}:accessToken`);
+  }
+
+  setAccessToken(accessToken) {
+    // Add the access token to the storage
+    return AsyncStorage.setItem(
+      `${this.namespace}:accessToken`,
+      JSON.stringify(accessToken)
+    );
+  }
+
+  removeAccessToken() {
+    // Remove the access token from the storage
+    return AsyncStorage.removeItem(`${this.namespace}:accessToken`);
+  }
