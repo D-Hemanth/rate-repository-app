2e58c406e5de9e80d37f4ffe6fc21453712d50cb
Hemanth-D <hemanthh531@gmail.com>
2022-10-18 12:08:56 +0530

    part10 10.16: Complete sign out

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -4,7 +4,7 @@ import useAuthStorage from './useAuthStorage';
 
 const useSignIn = () => {
   const authStorage = useAuthStorage();
-  // use apolloClient to use resetStore() method & remove all AsyncStorage data on the users device
+  // use apolloClient to use resetStore() method & remove all AsyncStor-age cache data from previous login on the users device
   const apolloClient = useApolloClient();
 
   // useSignIn hook that sends the authenticate mutation using the useMutation hook
@@ -18,8 +18,16 @@ const useSignIn = () => {
     // call the mutate function here with the right arguments which gives output { data, loading, error }
     // use mutate function query from useMutation for logging in a user who's data is already in gql server
     const { data } = await mutate({ variables: { username, password } });
-    await authStorage.setAccessToken(data.authenticate.accessToken);
-    apolloClient.resetStore();
+
+    if (data.authenticate.accessToken) {
+      await authStorage.setAccessToken(data.authenticate.accessToken);
+      // you have stored the user's access token to the storage, you should reset the Apollo Client's store.
+      // This will clear the Apollo Client's cache and re-execute all ac-tive queries.
+      apolloClient.resetStore();
+
+      // If you want to print or log your AsyncStorage in the console on React Native Debugger, you can do so easily with the following command
+      // console.log(showAsyncStorageContentInDev());
+    }
 
     // console.log('Logged in user accessToken', data);
     return data;
