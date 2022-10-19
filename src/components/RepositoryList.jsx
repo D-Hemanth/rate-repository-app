a3a872f1672f450347ee9f5ecff3f8a30cbb1cf9
Hemanth-D <hemanthh531@gmail.com>
2022-10-19 20:33:19 +0530

    part10 10.19: Add /:id route to view singleRepository item & use pressable to make repositoryItem go to /:id route

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,7 @@
-import { FlatList, View, StyleSheet } from 'react-native';
+import { FlatList, View, StyleSheet, Pressable } from 'react-native';
 import RepositoryItem from './RepositoryItem';
 import useRepositories from '../hooks/useRepositories';
+import { useNavigate } from 'react-router-native';
 
 const styles = StyleSheet.create({
   separator: {
@@ -12,13 +13,26 @@ const ItemSeparator = () => <View style={styles.separator} />;
 
 // export RepositoryListContainer having only the pure code without graphql query so that we can use it in testing
 export const RepositoryListContainer = ({ repositories }) => {
+  const navigate = useNavigate();
+
   // Since the data is paginated in a common cursor based pagination format. The actual repository data is behind the node key in the edges array.
   // Get the nodes from the edges array
   const repositoryNodes = repositories
     ? repositories.edges.map((edge) => edge.node)
     : [];
 
-  const renderItem = ({ item }) => <RepositoryItem item={item} />;
+  const singleRepoInfo = (item) => {
+    // console.log('singleRepoInfo item value', item);
+    navigate(`/${item.id}`);
+  };
+
+  const renderItem = ({ item }) => {
+    return (
+      <Pressable onPress={() => singleRepoInfo(item)}>
+        <RepositoryItem item={item} />
+      </Pressable>
+    );
+  };
 
   return (
     <FlatList
