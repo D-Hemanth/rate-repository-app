7ec5a10aad0c5d05e145d381f02fabf76369f493
Hemanth-D <hemanthh531@gmail.com>
2022-10-21 15:53:10 +0530

    part10 10.21: Create hook useCreateReview to use useMutation query to add Reviews

---
+++ src/hooks/useCreateReview.js
@@ -0,0 +1,25 @@
+import { useMutation } from '@apollo/client';
+import { CREATE_REVIEW } from '../graphql/mutations';
+
+const useCreateReview = () => {
+  // useMutation query to createReview mutate function
+  const [createReview, result] = useMutation(CREATE_REVIEW, {
+    onError: (error) => {
+      console.log(error.graphQLErrors[0]);
+    },
+  });
+
+  const addReview = async ({ ownerName, repositoryName, rating, text }) => {
+    // use the createReview mutation function to add a review with useMutation which gives output { data, loading, error }
+    const { data } = await createReview({
+      variables: { ownerName, repositoryName, rating: Number(rating), text },
+    });
+    console.log('useCreateReview mutate data', data);
+
+    return data;
+  };
+
+  return [addReview, result];
+};
+
+export default useCreateReview;
