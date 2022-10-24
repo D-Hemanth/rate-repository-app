c46752f6d39730b4fd0c17d2144a6200bcb2a925
Hemanth-D <hemanthh531@gmail.com>
2022-10-24 11:18:05 +0530

    part10 10.24: Modify repositoryList to add Searchbar & use react class, render ListHeaderComponent with debounce

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,7 +3,8 @@ import RepositoryItem from './RepositoryItem';
 import useRepositories from '../hooks/useRepositories';
 import { useNavigate } from 'react-router-native';
 import { Picker } from '@react-native-picker/picker';
-import { useState } from 'react';
+import { Searchbar } from 'react-native-paper';
+import { useDebounce } from 'use-debounce';
 
 const styles = StyleSheet.create({
   separator: {
@@ -14,45 +15,32 @@ const styles = StyleSheet.create({
     backgroundColor: '#C4CAD2',
   },
 });
-// #C4CAD2
+
 const ItemSeparator = () => <View style={styles.separator} />;
 
 // export RepositoryListContainer having only the pure code without graphql query so that we can use it in testing
-export const RepositoryListContainer = ({
-  repositories,
-  orderDirection,
-  setOrderDirection,
-}) => {
-  const navigate = useNavigate();
+export class RepositoryListContainer extends React.Component {
+  renderHeader = () => {
+    // this.props contains the component's props
+    const { orderDirection, setOrderDirection, searchQuery, setSearchQuery } =
+      this.props;
 
-  // Since the data is paginated in a common cursor based pagination format. The actual repository data is behind the node key in the edges array.
-  // Get the nodes from the edges array
-  const repositoryNodes = repositories
-    ? repositories.edges.map((edge) => edge.node)
-    : [];
+    const onChangeSearch = (query) => setSearchQuery(query);
 
-  const singleRepoInfo = (item) => {
-    // console.log('singleRepoInfo item value', item);
-    navigate(`/${item.id}`);
-  };
-
-  const renderItem = ({ item }) => {
-    return (
-      <Pressable onPress={() => singleRepoInfo(item)}>
-        <RepositoryItem item={item} />
-      </Pressable>
-    );
-  };
-
-  // sortingRepositories using the picker package & orderDirection useState hook
-  const sortingRepositories = () => {
+    // use the picker package & orderDirection useState hook to filter the repositories by their rating
     return (
       <View style={styles.picker}>
+        <Searchbar
+          placeholder="Search"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+        />
         <Picker
           selectedValue={orderDirection}
           onValueChange={(itemValue) =>
             setOrderDirection(itemValue || undefined)
           }
+          prompt="Select ordering criteria"
         >
           <Picker.Item label="Latest repositories" value="" />
           <Picker.Item label="Highest rated repositories" value="DESC" />
@@ -62,27 +50,62 @@ export const RepositoryListContainer = ({
     );
   };
 
-  return (
-    <FlatList
-      data={repositoryNodes}
-      ItemSeparatorComponent={ItemSeparator}
-      renderItem={renderItem}
-      ListHeaderComponent={sortingRepositories}
-    />
-  );
-};
+  render() {
+    const { repositories, navigate } = this.props;
+
+    // Since the data is paginated in a common cursor based pagination format. The actual repository data is behind the node key in the edges array.
+    // Get the nodes from the edges array
+    const repositoryNodes = repositories
+      ? repositories.edges.map((edge) => edge.node)
+      : [];
+
+    const singleRepoInfo = (item) => {
+      // console.log('singleRepoInfo item value', item);
+      navigate(`/${item.id}`);
+    };
+
+    const renderItem = ({ item }) => {
+      return (
+        <Pressable onPress={() => singleRepoInfo(item)}>
+          <RepositoryItem item={item} />
+        </Pressable>
+      );
+    };
+
+    return (
+      <FlatList
+        data={repositoryNodes}
+        ItemSeparatorComponent={ItemSeparator}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        ListHeaderComponent={this.renderHeader}
+      />
+    );
+  }
+}
 
 // get data from the rate repository api server backend using useRepositories() function component
 const RepositoryList = () => {
   // orderDirection hook to filter the repositories by their orderDirection of Rating values
   const [orderDirection, setOrderDirection] = useState(undefined);
-  const { repositories } = useRepositories({ orderDirection });
+  // use useState for filtering the reviewed repositories list based on a keyword/query parameter
+  const [searchQuery, setSearchQuery] = useState('');
+  // To avoid a multitude of unnecessary requests while the user types the keyword fast, only pick the latest input after a short delay using useDebounce
+  const [debouncedSearchQuery] = useDebounce(searchQuery, 500);
+  const { repositories } = useRepositories({
+    orderDirection,
+    debouncedSearchQuery,
+  });
+  const navigate = useNavigate();
 
   return (
     <RepositoryListContainer
       repositories={repositories}
       orderDirection={orderDirection}
       setOrderDirection={setOrderDirection}
+      setSearchQuery={setSearchQuery}
+      searchQuery={searchQuery}
+      navigate={navigate}
     />
   );
 };
