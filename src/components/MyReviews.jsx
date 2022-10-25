10bd06c1e26ad8b68c21330b4e3c8ee865f0056c
Hemanth-D <hemanthh531@gmail.com>
2022-10-25 11:17:14 +0530

    part10 10.27: Modify MyReviews component to show delete, view repo buttons with alert msg on pressing them

--- src/components/MyReviews.jsx
+++ src/components/MyReviews.jsx
@@ -1,9 +1,12 @@
-import { View, FlatList, StyleSheet } from 'react-native';
+import { View, FlatList, StyleSheet, Alert } from 'react-native';
 import { GET_CURRENT_USER } from '../graphql/queries';
 import { useQuery } from '@apollo/client';
 import Text from './Text';
 import theme from '../theme';
 import { format } from 'date-fns';
+import Button from './Button';
+import useDeleteReview from '../hooks/useDeleteReview';
+import { useNavigate } from 'react-router-native';
 
 const styles = StyleSheet.create({
   separator: {
@@ -16,6 +19,7 @@ const styles = StyleSheet.create({
   viewCardItemA: {
     padding: 15,
     marginRight: 60,
+    paddingBottom: 0,
   },
   ratingStyle: {
     marginTop: 15,
@@ -28,27 +32,107 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderColor: theme.backgroundColors.backgroundBlueColor,
   },
+  deleteButton: {
+    margin: 20,
+    backgroundColor: theme.colors.error, // background color red
+    width: '100%',
+    alignSelf: 'center',
+  },
+  repoButton: {
+    margin: 15,
+    backgroundColor: theme.backgroundColors.backgroundBlueColor,
+    width: '100%',
+    alignSelf: 'center',
+  },
+  buttonText: {
+    color: 'white',
+  },
+  viewCardB: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    backgroundColor: theme.backgroundColors.backgroundMainColor,
+  },
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-const ReviewItem = ({ review }) => {
+const ReviewItem = ({ review, deleteReview, refetch, navigate }) => {
+  const onDelete = async () => {
+    const id = review.id;
+    // console.log('delete review id', id);
+
+    await deleteReview({ id });
+    refetch();
+  };
+
+  // confirmation window . If the user confirms the deletion, the review is deleted. Otherwise, the deletion is discarded.
+  const showAlert = () => {
+    Alert.alert(
+      'Delete review',
+      'Are you sure you want to delete this review?',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => {
+            console.log('Alert window cancel clicked');
+          },
+          style: 'cancel',
+        },
+        {
+          text: 'Delete',
+          onPress: () => {
+            onDelete();
+          },
+        },
+      ],
+      {
+        cancelable: true, // you can cancel the alert dialog box by clicking cancel button outside of the alert dialog box
+      }
+    );
+  };
+
+  const onPressShow = () => {
+    navigate(`/${review.repositoryId}`, { replace: true });
+  };
+
   // Single review item
   return (
-    <View style={styles.viewCard}>
-      <View style={styles.ratingStyle}>
-        <Text color="primary" fontSize="subheading" fontWeight="bold">
-          {review.rating}
-        </Text>
+    <View>
+      <View style={styles.viewCard}>
+        <View style={styles.ratingStyle}>
+          <Text color="primary" fontSize="subheading" fontWeight="bold">
+            {review.rating}
+          </Text>
+        </View>
+        <View style={styles.viewCardItemA}>
+          <Text fontSize="subheading" fontWeight="bold">
+            {review.repository.fullName}
+          </Text>
+          <Text color="textSecondary">
+            {format(new Date(review.createdAt), 'dd.MM.yyyy')}
+          </Text>
+          <Text>{review.text}</Text>
+        </View>
       </View>
-      <View style={styles.viewCardItemA}>
-        <Text fontSize="subheading" fontWeight="bold">
-          {review.repository.fullName}
-        </Text>
-        <Text color="textSecondary">
-          {format(new Date(review.createdAt), 'dd.MM.yyyy')}
-        </Text>
-        <Text>{review.text}</Text>
+      <View style={styles.viewCardB}>
+        <Button onPress={onPressShow} style={styles.repoButton}>
+          <Text
+            fontSize="subheading"
+            fontWeight="bold"
+            style={styles.buttonText}
+          >
+            View repository
+          </Text>
+        </Button>
+        <Button onPress={showAlert} style={styles.deleteButton}>
+          <Text
+            fontSize="subheading"
+            fontWeight="bold"
+            style={styles.buttonText}
+          >
+            Delete review
+          </Text>
+        </Button>
       </View>
     </View>
   );
@@ -56,10 +140,12 @@ const ReviewItem = ({ review }) => {
 
 const MyReviews = () => {
   // use query GET_CURRENT_USER to check if the user is authenticated i.e. has access token or not authenticated i.e. query returns null
-  const { data, loading } = useQuery(GET_CURRENT_USER, {
+  const { data, loading, refetch } = useQuery(GET_CURRENT_USER, {
     fetchPolicy: 'cache-and-network',
     variables: { includeReviews: true },
   });
+  const [deleteReview] = useDeleteReview();
+  const navigate = useNavigate();
 
   if (loading) return <Text>Loading...</Text>;
   // console.log('query GET_CURRENT_USER to get authenticated user details with reviews', data);
@@ -74,7 +160,14 @@ const MyReviews = () => {
     <>
       <FlatList
         data={reviews}
-        renderItem={({ item }) => <ReviewItem review={item} />}
+        renderItem={({ item }) => (
+          <ReviewItem
+            review={item}
+            deleteReview={deleteReview}
+            refetch={refetch}
+            navigate={navigate}
+          />
+        )}
         keyExtractor={({ id }) => id}
         ItemSeparatorComponent={ItemSeparator}
       />
